a5567b93a8b03b5da9874084cead2141e2940a4a
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-08-09 22:16:38 -0700

    finished schema file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Ctable = require("console.table");
 
 const connection = mysql.createConnection({
     host: 'localhost',
--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,26 @@ DROP DATABASE IF EXISTS employeesDB;
 CREATE DATABASE employeesDB;
 
 USE employeesDB;
+
+CREATE TABLE Department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  manager_id INT NULL,
+  role_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(9.4) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
