f0edae3f750fcefbaf523a5d869c71ae1c47696f
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-08-09 22:45:24 -0700

    started with the first function

--- server.js
+++ server.js
@@ -10,3 +10,75 @@ const connection = mysql.createConnection({
     database: 'employeesDB'
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`
+    ________________________________________________________
+
+    ╔═══╗     ╔╗                ╔═╗╔═╗
+    ║╔══╝     ║║                ║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗  ║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣  ║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣  ║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝  ╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+           ║║      ╔═╝║                       ╔═╝║
+           ╚╝      ╚══╝                       ╚══╝
+
+    ________________________________________________________`)
+
+    firstPrompt();
+});
+
+function firstPrompt() {
+
+    inquirer
+        .prompt({
+            type: "list",
+            name: "Initial Options",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update a Role",
+                "End"]
+        })
+        .then(function ({ task }) {
+            switch (task) {
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "Add a Department":
+                    addDepartment();
+                    break;
+
+                case "Add a Role":
+                    addRole();
+                    break;
+
+                case "Add an Employee":
+                    addEmployee();
+                    break;
+
+                case "Update a Role":
+                    updateRole();
+                    break;
+
+                case "End":
+                    connection.end();
+                    break;
+            }
+        });
+}
